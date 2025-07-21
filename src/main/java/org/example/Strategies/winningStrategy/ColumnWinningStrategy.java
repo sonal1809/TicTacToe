3fbf72b5e12c862a2d524db069b00594fc085872
sonal1809 <sonalkumar181997@gmail.com>
2025-07-21 08:59:15 +0530

    Added winning strategy

--- src/main/java/org/example/Strategies/winningStrategy/ColumnWinningStrategy.java
+++ src/main/java/org/example/Strategies/winningStrategy/ColumnWinningStrategy.java
@@ -3,10 +3,26 @@ package org.example.Strategies.winningStrategy;
 import org.example.models.Board;
 import org.example.models.Move;
 
+import java.util.HashMap;
+
 public class ColumnWinningStrategy implements WinningStrategy {
+    private HashMap<Integer , HashMap<Character , Integer>> colMaps = new HashMap<>();
 
     @Override
     public boolean checkWinner(Board board, Move move) {
-        return false;
+        int col = move.getCell().getCol();
+        char aChar = move.getPlayer().getSymbol().getaChar();
+
+        if(!colMaps.containsKey(col)){
+            colMaps.put(col , new HashMap<>());
+        }
+
+        HashMap<Character , Integer> currColMaps = colMaps.get(col);
+        if(!currColMaps.containsKey(aChar)){
+            currColMaps.put(aChar , 0);
+        }
+
+        currColMaps.put(aChar , currColMaps.get(aChar) + 1);
+        return currColMaps.get(aChar).equals(board.getDimension());
     }
 }
--- src/main/java/org/example/Strategies/winningStrategy/DiagonalWinningStrategy.java
+++ src/main/java/org/example/Strategies/winningStrategy/DiagonalWinningStrategy.java
@@ -3,10 +3,42 @@ package org.example.Strategies.winningStrategy;
 import org.example.models.Board;
 import org.example.models.Move;
 
+import java.util.HashMap;
+
 public class DiagonalWinningStrategy implements WinningStrategy{
+    // 2 diagonal
+    private final HashMap<Character , Integer> leftDiagonalMap = new HashMap<>(); // Starting from 0,0
+    private final HashMap<Character , Integer> rightDiagonalMap = new HashMap<>(); // Starting from 0,n-1
 
     @Override
     public boolean checkWinner(Board board, Move move) {
+        int row = move.getCell().getRow();
+        int col = move.getCell().getRow();
+        Character aChar = move.getPlayer().getSymbol().getaChar();
+
+        // Condition for left diagonal - > row == col
+        if(row == col){
+            if(!leftDiagonalMap.containsKey(aChar)){
+                leftDiagonalMap.put(aChar , 0);
+            }
+            leftDiagonalMap.put(aChar , leftDiagonalMap.get(aChar) + 1);
+        }
+
+
+        // Condition for left diagonal - > row + col == N-1
+        if(row + col == board.getDimension()-1){
+            if(!rightDiagonalMap.containsKey(aChar)){
+                rightDiagonalMap.put(aChar , 0);
+            }
+            rightDiagonalMap.put(aChar , rightDiagonalMap.get(aChar) + 1);
+        }
+
+        if( (row == col && leftDiagonalMap.get(aChar) == board.getDimension()) ||
+                ( row + col == board.getDimension() -1 &&
+                        rightDiagonalMap.get(aChar) == board.getDimension() -1)) {
+            return true;
+        }
+
         return false;
     }
 }
